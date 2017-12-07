461fa93d679496d69d33003de48b15456578a011
jauxsh <joshua.jih.chang@gmail.com>
2017-12-06 19:12:04 -0800

    finished react-intro prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+  name: "Josh Chang",
+  luckyNumber: luckyNumber()
+};
+
+function luckyNumber() {
+    return Math.floor((Math.random() * 1000) +1)
+}
+
+let styles = {
+    color: 'lightpink',
+    fontWeight: 'bold'
+};
+
+function Greeting(props) {
+    console.log(props);
+    return <div className="container">
+            <h1 style={styles}>A 'suh {props.person.name}</h1>
+            <h2 className="text-muted" >Yo lucky number be {props.person.luckyNumber}!</h2>
+        </div>
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting person={user}/>,
     document.getElementById('root')
 );
