19ee1a468dbf881f0b85e4ec3f46c9e14adf58b5
jauxsh <joshua.jih.chang@gmail.com>
2017-12-12 21:50:44 -0800

    finished 1-4

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,21 @@
+import React from 'react';
+import ContactList from './contact_list';
+import ContactForm from './contact_form';
+import ContactCard from "./contact_card";
 
 
+const App = () => {
+    return(
+        <div className="container">
+            <h1 className="text-center">Address Book</h1>
+            <div className="row">
+                <div className="col-4">
+                    <ContactForm />
+                </div>
+                <ContactList />
+            </div>
+        </div>
+    )
+};
+
+export default App;
---
+++ src/data/contacts.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        firstName: "Jaux",
+        lastName: "Chang",
+        phone: "714-681-8590",
+        email: "joshua.jih.chang@gmail.com"
+    },
+    {
+        firstName: "Robin",
+        lastName: "Drake",
+        phone: "212-365-4854",
+        email: "robin@batman.com"
+    },
+    {
+        firstName: "Cayde-6",
+        lastName: "n/a",
+        phone: "--cannot reach cell on Traveler, earth cell towers went down centries ago...--",
+        email: "bestHunter@vanguard.com"
+    },
+    {
+        firstName: "Ikora",
+        lastName: "Rey",
+        phone: "212-654-8754",
+        email: "ikora@vanguard.com"
+    }
+];
+
