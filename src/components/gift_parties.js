a5ae4eac44c09945fc2af5f145f34246d7cdd688
jauxsh <joshua.jih.chang@gmail.com>
2017-12-18 18:30:07 -0800

    finishe react-routing prototype

---
+++ src/components/gift_parties.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Gifts & Parties</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt non odio pariatur.</p>
+        </div>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Welcome to this Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt non odio pariatur.</p>
+        </div>
+    )
+}
